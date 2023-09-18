b750b0495f9188dc81551014acdb8729a667ed88
motoki0724 <motokiokayasu@gmail.com>
2023-09-18 21:37:14 +0900

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Motoki Okayasu on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
