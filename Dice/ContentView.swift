d822525b9d5a66b4017f597ccc9ebf59e1121f70
motoki0724 <motokiokayasu@gmail.com>
2023-09-19 22:06:29 +0900

    decorate for image

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -11,6 +11,15 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "die.face.1")
+                //modifire
+                //画像の大きさを変更
+                .resizable()
+                //画像の縦横の比率を固定する
+                .scaledToFit()
+                //オブジェクトの大きさを変更
+                .frame(width: UIScreen.main.bounds.width/2)
+                //内側に余白をつける
+                .padding()
         }
         Button {
             print("ボタンが押されたよ")
