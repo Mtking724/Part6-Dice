871891aad67210b7191d745b425c3735a24174fc
motoki0724 <motokiokayasu@gmail.com>
2023-09-19 21:53:16 +0900

    first commit

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image(systemName: "die.face.1")
         }
-        .padding()
+        Button {
+            print("ボタンが押されたよ")
+        } label: {
+            Text("サイコロを振る")
+        }
     }
 }
 
