8ce1d7a27bedce1a7a32a0721b1c7bb81516a41b
motoki0724 <motokiokayasu@gmail.com>
2023-09-20 03:32:37 +0900

    complete part6

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var randomNumber = 1
-    @State var timer: Timer?
+    @State private var randomNumber = 1
+    @State private var timer: Timer?
+    @State private var isRolling = false
     
     var body: some View {
         VStack {
@@ -26,15 +27,7 @@ struct ContentView: View {
                 .padding()
             Spacer()
             Button {
-                print("ボタンが押されたよ")
-                //0.1秒ごとに出目を変更
-                timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in randomNumber = Int.random(in: 1...6)}
-                //0.5秒遅らせて処理を行う
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    //timerをストップ
-                    timer?.invalidate()
-                    timer = nil
-                }
+                playDice()
             } label: {
                 Text("サイコロを振る")
                 //modifire
@@ -47,12 +40,29 @@ struct ContentView: View {
                 //ボタンの角に丸みをつける
                     .cornerRadius(10)
             }
+            //ボタンを無効にさせるかどうかモディファイア
+            .disabled(isRolling)
             Spacer()
         }
     }
+    private func playDice() {
+        print("ボタンが押されたよ")
+        //ボタンを押してから0.5秒後にtrue
+        isRolling = true
+        //0.1秒ごとに出目を変更
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in randomNumber = Int.random(in: 1...6)}
+        //0.5秒遅らせて処理を行う
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            //timerをストップ
+            timer?.invalidate()
+            timer = nil
+            //0.5秒後にaflse
+            isRolling = false
+        }
+    }
 }
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
     }
 }
